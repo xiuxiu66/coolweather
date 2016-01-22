06b51ce4081d30a10fa3165544f16ab3dcaec3c5
Tony <tony@gmail.com>
2016-01-22 21:17:35 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/model/Province.java
@@ -0,0 +1,29 @@
+package model;
+
+import android.R.integer;
+
+public class Province {
+	private int id;
+	private String provinceName;
+	private String prvinceCode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getPrvinceCode() {
+		return prvinceCode;
+	}
+	public void setPrvinceCode(String prvinceCode) {
+		this.prvinceCode = prvinceCode;
+	}
+	
+	
+}
